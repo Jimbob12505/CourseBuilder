00f1637a7943ef4a2fe59a61a382f4fe3f029971
“Jimbob12505” <vatsal.12505@gmail.com>
2024-06-27 02:20:39 +0530

    Created slide_obj class

---
+++ slide_obj.py
@@ -0,0 +1,7 @@
+
+class Slide:
+    def __init__(self, title, bulletpoints, image, speakers_note):
+        self.title = title
+        self.bulletpoints = bulletpoints
+        self.image = image
+        self.speakers_note = speakers_note
